fb6f4a6f14e4f33859f491f7d224f11cab59cfef
Hatim Enezari <hatim.enezari@gmail.com>
2024-09-29 23:23:58 +0200

    Added a screen for the Promotion campaign: Made the lazy grid and modified the promotion domain entity.

--- app/domain/src/main/java/model/Promotion.kt
+++ app/domain/src/main/java/model/Promotion.kt
@@ -2,7 +2,10 @@ import datasource.local.entities.PromotionEntity
 
 data class Promotion(
     val id: Int,
-    val uri: String = "https://raw.githubusercontent.com/hatimenezari/TiniFood/247a754d322694b3a49885af944354fba3e2d2b1/promoImg.png"
+    val uri: String = "https://raw.githubusercontent.com/hatimenezari/TiniFood/247a754d322694b3a49885af944354fba3e2d2b1/promoImg.png",
+    val description: String = "Discount",
+    val startDate: String = "20/04/2024",
+    val endDate: String = "20/06/2024"
 )
 
 fun PromotionEntity.toDomainModel() = Promotion(id, uri)
